d1d6dc8664d069e035632df12235d6e9c182d569
GrishaCoder <grimy.grigory@gmail.com>
2024-01-09 17:01:28 +0000

    added time for events

--- events/add_event.php
+++ events/add_event.php
@@ -7,13 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $event_name = $_POST["eventName"];
     $event_desc = $_POST["eventDescription"];
     $event_date = $_POST["eventDate"];
+    $event_start_time = $_POST["eventStartTime"];
+    $event_end_time = $_POST["eventEndTime"];
 
 
-    $sql = "INSERT INTO events (event_name, event_desc, event_date) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO events (event_name, event_desc, event_date, event_start_time, event_end_time) VALUES (?, ?, ?, ?, ?)";
 
     // Use prepared statement to prevent SQL injection
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $event_name, $event_desc, $event_date);
+    $stmt->bind_param("sssss", $event_name, $event_desc, $event_date, $event_start_time, $event_end_time);
 
     // Execute the statement
     $stmt->execute();
