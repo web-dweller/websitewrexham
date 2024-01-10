7c4ea03ee12274fafd7627267d54c1437bdf1929
GrishaCoder <grimy.grigory@gmail.com>
2024-01-10 03:33:00 +0000

    added dicts and implemented text substitution

---
+++ languages/en.php
@@ -0,0 +1,43 @@
+<?php
+$lang = array(
+    'welcome' => 'Welcome, friend!',
+    'home' => 'Home',
+    'the_centre' => 'The Centre',
+    'about_us' => 'About us',
+    'contact' => 'Contact',
+    'supporters' => 'Supporters',
+    'welcome_text' => 'Saith Seren\'s aim is to provide a venue for Welsh speakers, learners and supporters of the language and culture to mingle and socialise. There are many events including Welsh and English entertainment, Welsh lessons or simply an opportunity to spend some leisurely time with friends.',
+    'support_seith_seren' => 'Support Seith Seren',
+    'support_seith_seren_text' => 'Become a member of Saith Seren for £10 a month, support us in our aim to promote Welsh language and culture.',
+    'sup_cont_btn' => 'Support / Contribute',
+    'opening_hours' => 'Opening hours',
+    'hours' =>  array("Monday - Thursday / 6:30 - 11:00", "Friday / 4:30 - 11:00", 'Saturday / 12:00 - Midnight'),
+    'upcoming_events' => 'Upcoming events',
+    'newsletter_signup' => 'Newsletter signup',
+    'firstName' => 'First Name',
+    'lastName' => 'Last Name',
+    'email' => 'Email',
+    'signup' => 'Sign up',
+    'the_pub' => 'The Pub',
+    'board' => 'Board',
+    'welsh_centre' => 'Y Saith Seren’ - Welsh Centre',
+    'welsh_centre_text' => 'The intention is to provide a focus for Welsh speakers, learners and supporters of the language
+    and culture. There are many events including Welsh and English entertainment, Welsh lessons or a
+    leisurely time with friends.',
+    'hours_centre' => array('Monday / Thursday 4:30 - 11:00', 'Friday / Saturday- 11:30 am - 1:00 am'),
+    'rooms' => 'Rooms available for rent',
+    'rooms_text' => array('We have 5 rooms available for rent from 1 hour or longer periods; ',
+     'Charities £5 per hour / Business Use £10 per hour ', 
+     'Full details - 01978 447006 / 7seren@gmail.com'),
+    'centre_photo1' => 'Two of our local brewers - Doug and Dave - at Saith Seren, Wrecsam to celebrate the new range of
+    real ales on offer behind the bar.',
+    'centre_photo2' => 'An astonishing performance dedicated to the tenth anniversary of Seith Seren. Be sure to check out our calendar, so you don\'t miss future events like this.',
+    'about_us1' => 'Saith Seren is a local cooperative in Wrexham town centre. The Welsh centre was established 10 years, unfortunately in 2015 the centre was at risk of closure. Due to the generosity of businesses and people across the globe the centre was saved from closure, people across the globe still help to keep the centre going. The centre will be growing and developing further in the future.',
+    'about_us2' => 'The story of the establishment of Saith Seren',
+    'about_us3' => 'Saith Seren, Wrexham\'s Welsh Centre, opened in 2012, following the National Eisteddfod\'s visit to Wrexham in Summer 2011. The \'Seven Stars\' pub had been vacant for 18 months, and local Councillor Marc Jones came up with the idea of re-open it as a Welsh Centre in order to build on the increased interest and pride in the language as a result of the Eisteddfod. He set up a Community Co-operative Company with a Management Board and shareholders. He persuaded Clwyd Alyn Housing Association to buy and renovate the listed building, which was then rented by the co-operative. \'Saith Seren\' opened at the end of January 2012.',
+    'about_us4' => 'The purpose of the Centre',
+    'about_us5' => '\'Saith\' is a centre for anyone who is proud to be, or interested in Welsh, to socialize and celebrate our culture. It provides a venue for Welsh language activities such as gigs, and sporting events on television with Welsh commentary on S4C. Clwb Clebran, every Thursday evening, gives Welsh language learners the opportunity to practice in an informal atmosphere outside the classroom. There are also many non-Welsh language activities e.g. darts, live bands on a Saturday night, but even on those nights, the Welsh spirit and culture is evident.',
+    'supporters_text' => 'These are the businesses that helped keep Saith Seren open. Please support them!'
+);
+echo json_encode($lang);
+?>
---
+++ locale.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function loadLanguage(lang) {
+    fetch(`languages/${lang}.php`)
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("home").textContent = data.home;
+        document.getElementById("the_centre").textContent = data.the_centre;
+        document.getElementById("contact").textContent = data.contact;
+        document.getElementById("supporters").textContent = data.supporters;
+        document.getElementById("about_us").textContent = data.about_us;
+        document.getElementById("the_pub").textContent = data.the_pub;
+        document.getElementById("board").textContent = data.board;
+        switch (window.location.pathname) {
+          case "/websitewrexham/index.html": {
+            document.getElementById("welcome").textContent = data.welcome;
+            document.getElementById("welcome_text").textContent =
+              data.welcome_text;
+            document.getElementById("support_seith_seren").textContent =
+              data.support_seith_seren;
+            document.getElementById("support_seith_seren_text").textContent =
+              data.support_seith_seren_text;
+            document.getElementById("sup_cont_btn").textContent =
+              data.sup_cont_btn;
+            document.getElementById("opening_hours").textContent =
+              data.opening_hours;
+            document.getElementById("hours").innerHTML =
+              data.hours.join("<br>");
+            document.getElementById("upcoming_events").textContent =
+              data.upcoming_events;
+            document.getElementById("newsletter_signup").textContent =
+              data.newsletter_signup;
+            document.getElementById("firstName").placeholder = data.firstName;
+            document.getElementById("lastName").placeholder = data.lastName;
+            document.getElementById("email").placeholder = data.email;
+            document.getElementById("signup").value = data.signup;
+            break;
+          }
+          case "/websitewrexham/the-centre.html": {
+            document.getElementById("welsh_centre").textContent =
+              data.welsh_centre;
+            document.getElementById("welsh_centre_text").textContent =
+              data.welsh_centre_text;
+            document.getElementById("support_seith_seren").textContent =
+              data.support_seith_seren;
+            document.getElementById("support_seith_seren_text").textContent =
+              data.support_seith_seren_text;
+            document.getElementById("sup_cont_btn").textContent =
+              data.sup_cont_btn;
+            document.getElementById("hours_centre").innerHTML =
+              data.hours_centre.join("<br>");
+            document.getElementById("opening_hours").textContent =
+              data.opening_hours;
+            document.getElementById("rooms_text").innerHTML =
+              data.rooms_text.join("<br>");
+            document.getElementById("rooms").textContent = data.rooms;
+
+            document.getElementById("centre_photo1").textContent =
+              data.centre_photo1;
+            document.getElementById("centre_photo2").textContent =
+              data.centre_photo2;
+            break;
+          }
+          case "/websitewrexham/about-us.html": {
+            document.getElementById("about_us1").textContent = data.about_us1;
+            document.getElementById("about_us1").innerHTML = document
+              .getElementById("about_us1")
+              .innerHTML.replace("Saith Seren", "<b>Saith Seren</b>");
+            document.getElementById("about_us2").textContent = data.about_us2;
+            document.getElementById("about_us3").textContent = data.about_us3;
+            document.getElementById("about_us4").textContent = data.about_us4;
+            document.getElementById("about_us5").textContent = data.about_us5;
+            break;
+          }
+          case "/websitewrexham/contact.html": {
+            document.getElementById("contact1").textContent = data.contact;
+            break;
+          }
+          case "/websitewrexham/supporters.html": {
+            document.getElementById("supporters_text").textContent =
+              data.supporters_text;
+            break;
+          }
+        }
+      });
+  }
+
+  const defaultLang = navigator.language.startsWith("en") ? "en" : "en"; //TODO logic with button to switch lang
+  loadLanguage(defaultLang);
+});
